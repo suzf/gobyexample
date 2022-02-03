3186ae202cdfe7622ff6182bec70b838e43a0f86
suzf <i@suzf.net>
2022-02-03 18:02:27 +0800

    +add spawning processes example

---
+++ 73-spawning-processes/spawning-processes.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "io"
+    "fmt"
+    "os/exec"
+)
+
+func main() {
+    dateCmd := exec.Command("date")
+    dateOut, err := dateCmd.Output()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("> date")
+    fmt.Println(string(dateOut))
+
+    grepCmd := exec.Command("grep", "hello")
+    grepIn, _ := grepCmd.StdinPipe()
+    grepOut, _ := grepCmd.StdoutPipe()
+    grepCmd.Start()
+    grepIn.Write([]byte("hello grep\ngoodbye grep"))
+    grepIn.Close()
+    grepBytes, _ := io.ReadAll(grepOut)
+    grepCmd.Wait()
+    fmt.Println("> grep hello")
+    fmt.Println(string(grepBytes))
+
+    lsCmd := exec.Command("bash", "-c", "ls -alh")
+    lsOut, err := lsCmd.Output()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("> ls -alh")
+    fmt.Println(string(lsOut))
+}
